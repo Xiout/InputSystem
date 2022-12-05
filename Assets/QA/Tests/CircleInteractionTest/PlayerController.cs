7eb7b842914268c5e6ef7c6a793fe031cfb6ba06
Alexia Dauvergne <71349095+Xiout@users.noreply.github.com>
2022-12-05 17:51:50 +0100

    Create and register CircleInteraction from HoldInteraction
    ADD
    -CircleInteraction custom interaction (copy from HoldInteraction)
    -Add "RegisterInteraction" method that will be called thanks to InitializeOnLoad declarative tag
    -[SCENE] Create scene for testing CircleInteraction

---
+++ Assets/QA/Tests/CircleInteractionTest/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 movementVector; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTurn(InputValue movementValue)
+    {
+       
+    }
+}
