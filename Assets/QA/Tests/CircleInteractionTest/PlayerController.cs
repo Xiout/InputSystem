4ca620f7d74756e593a1354b8836b63de2997100
Alexia Dauvergne <71349095+Xiout@users.noreply.github.com>
2022-12-29 20:49:16 +0100

    Create Visual Aid for Debug
    ADD
    -In PlayerController, add listener for "performed" "started" and "canceled" callback event from InputAction "Turn"
    -When an InputAction "Turn" is started, draw a orange point on the screen for each collected point, when the InputAction "Turn" is performed, draw a green point on the screen for each newly colelcted point. Wipe out previous drawn points at the beginning of each InputAction.

--- Assets/QA/Tests/CircleInteractionTest/PlayerController.cs
+++ Assets/QA/Tests/CircleInteractionTest/PlayerController.cs
@@ -8,6 +8,15 @@ public class PlayerController : MonoBehaviour
     private bool _isTurn;
     private CircleAction controls;
 
+    public GameObject Prefab_PointOrange;
+    public GameObject Prefab_PointGreen;
+    public GameObject Prefab_PointGrey;
+
+    private GameObject DebugGO;
+
+    private bool isStarted;
+    private bool isPerformed;
+
     void Awake()
     {
         controls = new CircleAction();
@@ -16,7 +25,10 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        //controls.Player.Turn.performed += Turn;
+        DebugGO = GameObject.Find("DEBUG");
+        controls.Player.Turn.started += ctx => OnTurnStarted(ctx);
+        controls.Player.Turn.performed += ctx => OnTurnPerformed(ctx);
+        controls.Player.Turn.canceled += ctx => OnTurnCanceled(ctx);
     }
 
     private void OnEnable()
@@ -31,6 +43,24 @@ public class PlayerController : MonoBehaviour
             controls.Disable();
     }
 
+    void OnTurnStarted(InputAction.CallbackContext ctx)
+    {
+        isStarted = true;
+        GameObject.Destroy(DebugGO);
+        DebugGO = new GameObject();
+        DebugGO.name = "DEBUG";
+    }
+
+    void OnTurnPerformed(InputAction.CallbackContext ctx)
+    {
+        isPerformed = true;
+    }
+
+    void OnTurnCanceled(InputAction.CallbackContext ctx)
+    {
+        isStarted = false;
+        isPerformed = false;
+    }
 
     void Update()
     {
@@ -38,5 +68,28 @@ public class PlayerController : MonoBehaviour
         {
             gameObject.transform.RotateAround(gameObject.transform.position, Vector3.up, 30);
         }
+        
+        Vector2 mousePos2D = controls.Player.Turn.ReadValue<Vector2>();
+        if(mousePos2D != null && isStarted)
+        {
+            Vector3 mousePos3D = new Vector3(mousePos2D.x, mousePos2D.y, Camera.main.nearClipPlane + 1);
+            Vector3 worldPos3D = Camera.main.ScreenToWorldPoint(mousePos3D);
+
+            GameObject newPoint;
+            if (isPerformed)
+            {
+                newPoint = GameObject.Instantiate(Prefab_PointGreen);
+                newPoint.transform.position = worldPos3D;
+                newPoint.transform.Translate(0, 0, -0.01f);
+            }
+            else
+            {
+                newPoint = GameObject.Instantiate(Prefab_PointOrange);
+                newPoint.transform.position = worldPos3D;
+            }
+
+            newPoint.transform.SetParent(DebugGO.transform);
+        }
+       
     }
 }
